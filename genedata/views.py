2ef7eca1ab04a31c6c546c5c20d8ea442186d79d
yslaoui <yslaouiyassine@gmail.com>
2023-11-29 12:26:46 +0100

    Adding list of all genes in '' route and gene specific table in gene/id

--- genedata/views.py
+++ genedata/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    genes = Gene.objects.all()
+    return render(request, 
+                  'genedata/index.html', 
+                  {'genes': genes})
+
+def gene(request, pk):
+    gene = Gene.objects.get(id = pk)
+    return render(request, 
+                  'genedata/gene.html', 
+                  {'gene': gene})
+
